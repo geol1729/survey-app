7f267e16f97eef52820313803c60ad3d2e53a1d2
George Levasser <geol1729@gmail.com>
2020-03-02 10:24:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/greeting', (req, res) => {
+  res.send({
+    hi: 'there',
+    greeting: 'to you',
+    what: 'We love Star Trek',
+    alt: 'we do too'
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
