205b604660aeb8d7dca221c3af862523cb7db4a2
George Levasser <geol1729@gmail.com>
2020-03-02 15:50:42 -0500

    problems deploying to Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/greeting', (req, res) => {
+app.get('/', (req, res) => {
   res.send({
     hi: 'there',
     greeting: 'to you',
